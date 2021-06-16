c36284b0dfa44b746ddf4596ac8e6c8f2d271ee0
joaofzm <joao.fzm@hotmail.com>
2021-06-16 19:51:14 -0300

    Create TrapCard.java

---
+++ src/br/com/joaofzm15/linkVrains/cards/TrapCard.java
@@ -0,0 +1,11 @@
+package br.com.joaofzm15.linkVrains.cards;
+
+import br.com.joaofzm15.linkVrains.decks.Deck;
+
+public class TrapCard extends Card {
+
+	public TrapCard(Deck deck, String iconCode) {
+		super(deck, iconCode);
+	}
+
+}
