3e719b5f9a036c4a37605431b46f109fbdef98c9
joaofzm <joao.fzm@hotmail.com>
2021-06-16 19:51:17 -0300

    Create SpellCard.java

---
+++ src/br/com/joaofzm15/linkVrains/cards/SpellCard.java
@@ -0,0 +1,11 @@
+package br.com.joaofzm15.linkVrains.cards;
+
+import br.com.joaofzm15.linkVrains.decks.Deck;
+
+public class SpellCard extends Card {
+
+	public SpellCard(Deck deck, String iconCode) {
+		super(deck, iconCode);
+	}
+
+}
