5347c19ab61917e4a8d2951455a0d90be0bc4fa8
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:45:42 -0300

    Update MonsterCard.java

--- src/br/com/joaofzm15/linkVrains/cards/MonsterCard.java
+++ src/br/com/joaofzm15/linkVrains/cards/MonsterCard.java
@@ -3,70 +3,21 @@ package br.com.joaofzm15.linkVrains.cards;
 import br.com.joaofzm15.linkVrains.decks.Deck;
 
 public class MonsterCard extends Card {
-	private int atk;
-	private int def;
-	private int level;
-	private String type;
-	private String attribute;
-	private boolean tuner = false;
+	private int power;
+	private int cost;
 
-	public MonsterCard(Deck deck,String iconCode, int atk, int def, int level, String type, String attribute, int tuner) {
+	public MonsterCard(Deck deck, String iconCode, int power, int cost) {
 		super(deck, iconCode);
-		this.atk = atk;
-		this.def = def;
-		this.level = level;
-		this.type = type;
-		this.attribute = attribute;
-		if (tuner == 1) {
-			this.tuner = true;
-		}
+		this.power = power;
+		this.cost = cost;
 	}
 
-	public int getAtk() {
-		return atk;
+	public int getPower() {
+		return power;
 	}
 
-	public void setAtk(int atk) {
-		this.atk = atk;
+	public int getCost() {
+		return cost;
 	}
 
-	public int getDef() {
-		return def;
-	}
-
-	public void setDef(int def) {
-		this.def = def;
-	}
-
-	public int getLevel() {
-		return level;
-	}
-
-	public void setLevel(int level) {
-		this.level = level;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getAttribute() {
-		return attribute;
-	}
-
-	public void setAttribute(String attribute) {
-		this.attribute = attribute;
-	}
-
-	public boolean isTuner() {
-		return tuner;
-	}
-
-	public void setTuner(boolean tuner) {
-		this.tuner = tuner;
-	}
 }
