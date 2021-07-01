f495bdae6fd2f411b4f1fe0cfd5f936b2d87d97d
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:47:15 -0300

    Update HandPanelButton.java

--- src/br/com/joaofzm15/linkVrains/gui/buttons/HandPanelButton.java
+++ src/br/com/joaofzm15/linkVrains/gui/buttons/HandPanelButton.java
@@ -1,94 +1,129 @@
 package br.com.joaofzm15.linkVrains.gui.buttons;
 
-import java.awt.Image;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-
 import br.com.joaofzm15.linkVrains.cards.Card;
+import br.com.joaofzm15.linkVrains.gui.animationsAndSFX.FixButtonLayoutThread;
 
-public class HandPanelButton implements ActionListener, MouseListener {
+public class HandPanelButton extends CardButton implements ActionListener, MouseListener {
+
+	private SummonButton summonButton;
+	private ActivateButton activateButton;
+	private PlaceButton placeButton;
+	
+	private Card card;
+	public Card getCard() {
+		return card;
+	}
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
+	private int power;
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	/*
+	 * All the cards on hand are represented by this class button.
+	 * 
+	 * While this class is called HandPanelButton, it's not a button and only
+	 * contains a button, as well as other properties. The button must be accessed
+	 * with "fieldButton.getButton()".
+	 * 
+	 * The buttons must be part of a HandPanel, and one is required as a parameter
+	 * in the constructor.
+	 */
 
 	private HandPanel handPanel;
 	public HandPanel getHandPanel() {
 		return handPanel;
 	}
 
-	private JButton button;
-	public JButton getButton() {
-		return button;
-	}
-	
-	private ImageIcon currentCardBigCard;
 
-	public ImageIcon getImageIconFromCard(Card card) {
-		ImageIcon temporaryImageIcon = new ImageIcon(
-				getClass().getClassLoader().getResource("cards/" + card.getIconCode() + ".jpg"));
-		return temporaryImageIcon;
-	}
-
-	public void setIcons(ImageIcon imageIcon) {
-		Image temporaryOriginalImage = imageIcon.getImage();
-		Image temporaryBigImage = temporaryOriginalImage.getScaledInstance((int) 271, (int) 395,java.awt.Image.SCALE_SMOOTH);
-		ImageIcon resizedBigImageIcon = new ImageIcon(temporaryBigImage);
-		this.currentCardBigCard = resizedBigImageIcon;
-		
-		Image temporaryMiniImage = temporaryOriginalImage.getScaledInstance((int) 77.6, (int) 113.4,java.awt.Image.SCALE_SMOOTH);
-		ImageIcon resizedMiniImageIcon = new ImageIcon(temporaryMiniImage);
-		this.getButton().setIcon(resizedMiniImageIcon);
-	}
-
+	/*
+	 * The constructor will automatically add the button to the HandPanel passed as
+	 * a parameter.
+	 */
 	public HandPanelButton(HandPanel handPanel) {
+		super();
 		this.handPanel = handPanel;
-		this.button = new JButton();
+		this.button.setLayout(new GridLayout(4, 0));
 		this.button.setBorder(null);
 		this.button.setBackground(null);
 		this.button.addActionListener(this);
 		this.button.addMouseListener(this);
 		this.getHandPanel().getPanel().add(this.button);
+
+		this.summonButton = new SummonButton(this);
+		this.activateButton = new ActivateButton(this);
+		this.placeButton = new PlaceButton(this);
+
+		handPanel.getButtonList().add(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == this.getButton()) {
-			System.out.println("Botão clickado");
+		for (int i = 0; i < handPanel.getButtonList().size(); i++) {
+			handPanel.getButtonList().get(i).removeSummonAndSetButtons();
+			handPanel.getButtonList().get(i).removeSummonAndSetButtons();
+			handPanel.getButtonList().get(i).getButton().requestFocus();
 		}
+		if (this.getPower() >= 0 && this.getPower() < 50000) {
+			this.getButton().add(summonButton.getButton());
+		} else if (this.getPower() < 100000) {
+			this.getButton().add(activateButton.getButton());
+		} else {
+			this.getButton().add(placeButton.getButton());
+		}
+		new Thread(new FixButtonLayoutThread(this.getHandPanel().getDuelFrame())).start();
+
 	}
 
+	/*
+	 * As the mouse enters the button, the big card stored in this class will update
+	 * the icon of the cardInfo label(from DuelFrame).
+	 */
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		if (this.currentCardBigCard != null) {
-			this.getHandPanel().getDuelFrame().getCardInfo().setIcon(this.currentCardBigCard);
+			this.getHandPanel().getDuelFrame().getCardInfo().setIcon(this.getCurrentCardBigCard());
 		}
 	}
 
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
 
+	}
+
+	public void removeSummonAndSetButtons() {
+		this.getButton().remove(summonButton.getButton());
+		this.getButton().remove(activateButton.getButton());
+		this.getButton().remove(placeButton.getButton());
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
-
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
