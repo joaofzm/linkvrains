c7e4b59be645bc49a41fd719b238389883d199d9
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:46:12 -0300

    Update DrawCardAnimation.java

--- src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/DrawCardAnimation.java
+++ src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/DrawCardAnimation.java
@@ -9,25 +9,24 @@ import br.com.joaofzm15.linkVrains.gui.DuelFrame;
 public class DrawCardAnimation implements Runnable {
 
 	private DuelFrame duelFrame;
-	private String soundLocation;
+	private String soundPath;
 	private SoundEffectConverter soundEffectConverter = new SoundEffectConverter();
 
 
 	public DrawCardAnimation(DuelFrame df) {
 		this.duelFrame = df;
-		soundLocation = "resources/sound effects/draw.wav";
+		soundPath = "resources/sound effects/draw.wav";
 	}
 
 	@Override
 	public void run() {
-		
 		int xPos = 1160;
 		int yPos = 580;
 		duelFrame.getDrawCardAnimationLabel().setBounds(xPos, yPos, (int) 77.76, (int) 113.4);
 		duelFrame.getDrawCardAnimationLabel().setBorder(BorderFactory.createLineBorder(Color.white));
 		duelFrame.getDrawCardAnimationLabel().setIcon(duelFrame.getGeneralIcons().getMiniCoverImageIcon());
 		duelFrame.getDrawCardAnimationLabel().setVisible(true);
-		soundEffectConverter.setFile(soundLocation);
+		soundEffectConverter.setFile(soundPath);
 		soundEffectConverter.play();
 		for (int i = 0; i < 26; i++) {
 				try {Thread.sleep(10);} catch (InterruptedException ex) {ex.printStackTrace();}
@@ -36,7 +35,12 @@ public class DrawCardAnimation implements Runnable {
 				duelFrame.getDrawCardAnimationLabel().setBounds(xPos, yPos, (int) 77.76, (int) 113.4);
 			}
 		duelFrame.getDrawCardAnimationLabel().setVisible(false);
-
+		try {
+			this.finalize();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
