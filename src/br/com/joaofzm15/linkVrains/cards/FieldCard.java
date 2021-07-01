6751840731d81f2eada814409767d88f279d6af4
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:45:39 -0300

    Create FieldCard.java

---
+++ src/br/com/joaofzm15/linkVrains/cards/FieldCard.java
@@ -0,0 +1,11 @@
+package br.com.joaofzm15.linkVrains.cards;
+
+import br.com.joaofzm15.linkVrains.decks.Deck;
+
+public class FieldCard extends Card {
+
+	public FieldCard(Deck deck, String iconCode) {
+		super(deck, iconCode);
+	}
+
+}
