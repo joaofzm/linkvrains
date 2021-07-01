051aa816c8f2b1cf588f50601755622d5c11606d
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:47:44 -0300

    Update LinkVrains.java

--- src/br/com/joaofzm15/linkVrains/gui/LinkVrains.java
+++ src/br/com/joaofzm15/linkVrains/gui/LinkVrains.java
@@ -5,12 +5,14 @@ import br.com.joaofzm15.linkVrains.gui.animationsAndSFX.MenuBackgroundMusic;
 public class LinkVrains {
 
 	public static void main(String[] args) {
-
+		
 		Thread menuBgMusic = new Thread(new MenuBackgroundMusic());
 		menuBgMusic.start();
 
 		new StartMenu();
 
+
 	}
+
 
 }
