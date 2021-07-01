95ea109925d268c2be77bb7ed1ec7c1b75e5cc37
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:46:34 -0300

    Create SummonAnimation.java

---
+++ src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/SummonAnimation.java
@@ -0,0 +1,32 @@
+package br.com.joaofzm15.linkVrains.gui.animationsAndSFX;
+
+import br.com.joaofzm15.linkVrains.gui.DuelFrame;
+
+public class SummonAnimation implements Runnable {
+
+	DuelFrame duelFrame;
+
+	public DuelFrame getDuelFrame() {
+		return duelFrame;
+	}
+
+	private SoundEffectConverter soundEffectConverter = new SoundEffectConverter();
+	String soundPath = "resources/sound effects/summon.wav";
+
+	public SummonAnimation(DuelFrame duelFrame, int xPos, int yPos) {
+		this.duelFrame = duelFrame;
+		duelFrame.getBloom().setBounds((int) xPos, (int) yPos, (int) 77, (int) 113);
+	}
+
+	@Override
+	public void run() {
+		soundEffectConverter.setFile(soundPath);
+		soundEffectConverter.play();
+		getDuelFrame().getBloom().setVisible(true);
+		try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+		getDuelFrame().getBloom().setVisible(false);
+	}
+
+
+
+}
