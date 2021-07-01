c5c769eb5c9b3d600a772a7e78d94eda7b89dedf
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:46:21 -0300

    Update MenuBackgroundMusic.java

--- src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/MenuBackgroundMusic.java
+++ src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/MenuBackgroundMusic.java
@@ -3,12 +3,18 @@ package br.com.joaofzm15.linkVrains.gui.animationsAndSFX;
 public class MenuBackgroundMusic implements Runnable {
 
 	private SoundEffectConverter soundEffectConverter = new SoundEffectConverter();
-	String soundLocation = "resources/sound effects/menu bgmusic.wav";
 
+	String soundPath = "resources/sound effects/menu bgmusic.wav";
+
+	/*
+	 * The Thread runs a infinity loop that sleeps for 194000 milliseconds, which is
+	 * the exact duration of the song, this way the song will run indefinitely,
+	 * restarting every time it ends.
+	 */
 	@Override
 	public void run() {
 		while (true) {
-			soundEffectConverter.setFile(soundLocation);
+			soundEffectConverter.setFile(soundPath);
 			soundEffectConverter.play();
 			try {
 				Thread.sleep(194000);
