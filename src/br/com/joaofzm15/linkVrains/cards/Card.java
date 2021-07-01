30eaa8ede63d1b58139685b6dea8fe0a81075485
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:45:34 -0300

    Update Card.java

--- src/br/com/joaofzm15/linkVrains/cards/Card.java
+++ src/br/com/joaofzm15/linkVrains/cards/Card.java
@@ -4,18 +4,32 @@ import br.com.joaofzm15.linkVrains.decks.Deck;
 
 public abstract class Card {
 
+	/*
+	 * This String is used to concatenate another String and form the file path by
+	 * the FieldButton class.
+	 * 
+	 * All card images are stored in the resources folder, and are named as a
+	 * number, (i.e. "3.jpg"). This variable should receive it's respective image
+	 * file number as a String (i.e. "3").
+	 */
 	private String iconCode;
 
+	/*
+	 * All cards must be part of a Deck. Deck contain two ArrayList, one to store
+	 * the main deck and one to store the extra deck. The constructor requires a
+	 * Deck as a parameter, and will automatically verify if the Card is a main deck
+	 * card or a extra deck card, and add it for that Deck appropriate ArrayList.
+	 */
 	private Deck deck;
+	public Deck getDeck() {
+		return deck;
+	}
 
 	public Card(Deck deck, String iconCode) {
 		this.deck = deck;
 		this.iconCode = iconCode;
-		if (this.getClass() != ExtraMonsterCard.class) {
-			this.deck.getDeckList().add(this);
-		} else {
-			this.deck.getExtraDeckList().add(this);
-		}
+		this.getDeck().getDeckArrayList().add(this);
+
 	}
 
 	public String getIconCode() {
@@ -26,5 +40,7 @@ public abstract class Card {
 	public String toString() {
 		return this.iconCode;
 	}
+
+
 
 }
