24ae9f7817c0234f9c04a33112443610ef2e7168
joaofzm <joao.fzm@hotmail.com>
2021-06-30 22:45:59 -0300

    Create AllZonesOccupiedWarning.java

---
+++ src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/AllZonesOccupiedWarning.java
@@ -0,0 +1,27 @@
+package br.com.joaofzm15.linkVrains.gui.animationsAndSFX;
+
+import br.com.joaofzm15.linkVrains.gui.DuelFrame;
+
+public class AllZonesOccupiedWarning implements Runnable {
+
+	private DuelFrame duelFrame;
+
+	public AllZonesOccupiedWarning(DuelFrame duelFrame) {
+		this.duelFrame = duelFrame;
+	}
+
+	@Override
+	public void run() {
+		new Thread(new ErrorSound()).start();
+		this.duelFrame.getWarningsLabel().setText("All monster zones are occupied!");
+		duelFrame.getWarningsLabel().setVisible(true);
+		try {
+			Thread.sleep(1100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		duelFrame.getWarningsLabel().setVisible(false);
+
+	}
+
+}
