e0921ddb7d1ef7cf5cd7070f93c067aaae63fad5
joaofzm <joao.fzm@hotmail.com>
2021-07-01 22:41:43 -0300

    Update AiTurns.java

--- src/br/com/joaofzm15/linkVrains/ai/AiTurns.java
+++ src/br/com/joaofzm15/linkVrains/ai/AiTurns.java
@@ -11,30 +11,46 @@ public class AiTurns {
 		this.duelFrame = duelFrame;
 	}
 
-	public void aiTurn2() {
+	public void aiTurn() {
 		duelFrame.getPlayerEnablerDisabler().disablePlayer();
 		new Thread(new FixButtonLayoutThread(duelFrame)).start();
 		new Thread() {
 			public void run() {
-				/// Turn actions here
+
+				for (int i = 0; i < duelFrame.getOpponentHandPanel().getButtonList().size(); i++) {
+					if (duelFrame.getOpponentHandPanel().getButtonList().get(i).getPower() < 50000) {
+						boolean sucesfullSummon = duelFrame.getAiActions().summon(duelFrame.getOpponentHandPanel().getButtonList().get(i));
+						if (sucesfullSummon) {
+							i--;
+							try {Thread.sleep(1200);} catch (InterruptedException e) {e.printStackTrace();}
+						}
+					}
+				}
+
+				for (int i = 0; i < duelFrame.getOpponentHandPanel().getButtonList().size(); i++) {
+					if (duelFrame.getOpponentHandPanel().getButtonList().get(i).getPower() >= 50000 && duelFrame.getOpponentHandPanel().getButtonList().get(i).getPower() < 100000) {
+						duelFrame.getAiActions().activate(duelFrame.getOpponentHandPanel().getButtonList().get(i));
+						i--;
+						try {Thread.sleep(1200);} catch (InterruptedException e) {e.printStackTrace();}
+					}
+				}
+
+				for (int i = 0; i < duelFrame.getOpponentHandPanel().getButtonList().size(); i++) {
+					if (duelFrame.getOpponentHandPanel().getButtonList().get(i).getPower() >= 100000) {
+						duelFrame.getAiActions().place(duelFrame.getOpponentHandPanel().getButtonList().get(i));
+						try {Thread.sleep(1200);} catch (InterruptedException e) {e.printStackTrace();}
+						break;
+					}
+				}
+
+				// Attacks
 				try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
 				duelFrame.getPlayerEnablerDisabler().enablePlayer();
 				duelFrame.passRound();
 			}
 		}.start();
 		
-	}
 
-	public void aiTurn4() {
-		System.out.println("Pinto 4");
-	}
-
-	public void aiTurn6() {
-		System.out.println("Pinto 6");
-	}
-
-	public void aiTurn8() {
-		System.out.println("Pinto 8");
 	}
 
 }
