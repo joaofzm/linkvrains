214cb9d6bee5df22efb70df848756adc897bf58a
joaofzm <joao.fzm@hotmail.com>
2021-07-02 23:08:25 -0300

    Update InsufficientManaWarning.java

--- src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/InsufficientManaWarning.java
+++ src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/InsufficientManaWarning.java
@@ -1,5 +1,7 @@
 package br.com.joaofzm15.linkVrains.gui.animationsAndSFX;
 
+import java.awt.Font;
+
 import br.com.joaofzm15.linkVrains.gui.DuelFrame;
 
 public class InsufficientManaWarning implements Runnable {
@@ -13,7 +15,8 @@ public class InsufficientManaWarning implements Runnable {
 	@Override
 	public void run() {
 		new Thread(new ErrorSound()).start();
-		this.duelFrame.getWarningsLabel().setText("Insufficient mana!");
+		this.duelFrame.getWarningsLabel().setFont(new Font("Impact", Font.PLAIN, 40));
+		this.duelFrame.getWarningsLabel().setText("         Insufficient mana!");
 		duelFrame.getWarningsLabel().setVisible(true);
 		try {
 			Thread.sleep(1100);
