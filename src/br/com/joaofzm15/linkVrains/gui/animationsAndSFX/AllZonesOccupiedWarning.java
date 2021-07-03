e97a5ca78591b2f106564bf24d2a47449b0a12b7
joaofzm <joao.fzm@hotmail.com>
2021-07-02 23:08:07 -0300

    Update AllZonesOccupiedWarning.java

--- src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/AllZonesOccupiedWarning.java
+++ src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/AllZonesOccupiedWarning.java
@@ -1,5 +1,7 @@
 package br.com.joaofzm15.linkVrains.gui.animationsAndSFX;
 
+import java.awt.Font;
+
 import br.com.joaofzm15.linkVrains.gui.DuelFrame;
 
 public class AllZonesOccupiedWarning implements Runnable {
@@ -13,6 +15,7 @@ public class AllZonesOccupiedWarning implements Runnable {
 	@Override
 	public void run() {
 		new Thread(new ErrorSound()).start();
+		this.duelFrame.getWarningsLabel().setFont(new Font("Impact", Font.PLAIN, 30));
 		this.duelFrame.getWarningsLabel().setText("All monster zones are occupied!");
 		duelFrame.getWarningsLabel().setVisible(true);
 		try {
