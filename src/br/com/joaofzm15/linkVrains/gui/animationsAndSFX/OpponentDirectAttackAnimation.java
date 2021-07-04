08e30c2c044548b3d345b3518ecf8fa022b252cc
joaofzm <joao.fzm@hotmail.com>
2021-07-04 00:01:25 -0300

    Update OpponentDirectAttackAnimation.java

--- src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/OpponentDirectAttackAnimation.java
+++ src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/OpponentDirectAttackAnimation.java
@@ -85,21 +85,21 @@ public class OpponentDirectAttackAnimation implements Runnable {
 		}
 		
 		if (this.button == duelFrame.getOpponentMonsterZone1().getButton()) {
-			this.duelFrame.getLabelTesteFire().setBounds((int) 910, (int) 155, (int) 1000, (int) 1130.4);
+			this.duelFrame.getImpactExplosionlabel().setBounds((int) 910, (int) 155, (int) 1000, (int) 1130.4);
 
 		} else if (this.button == duelFrame.getOpponentMonsterZone2().getButton()) {
-			this.duelFrame.getLabelTesteFire().setBounds((int) 796.6, (int) 155, (int) 1000, (int) 1130.4);
+			this.duelFrame.getImpactExplosionlabel().setBounds((int) 796.6, (int) 155, (int) 1000, (int) 1130.4);
 
 		} else if (this.button == duelFrame.getOpponentMonsterZone3().getButton()) {
-			this.duelFrame.getLabelTesteFire().setBounds((int) 683.2, (int) 155, (int) 1000, (int) 1130.4);
+			this.duelFrame.getImpactExplosionlabel().setBounds((int) 683.2, (int) 155, (int) 1000, (int) 1130.4);
 		}
 
 		soundEffectConverter.setFile(soundLocation2);
 		soundEffectConverter.play();
-		duelFrame.getLabelTesteFire().setVisible(true);
+		duelFrame.getImpactExplosionlabel().setVisible(true);
 		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
 		yPos += 30; // Cancel Recoil
-		duelFrame.getLabelTesteFire().setVisible(false);
+		duelFrame.getImpactExplosionlabel().setVisible(false);
 
 		// Back
 		for (int i = 0; i < 60; i++) {
