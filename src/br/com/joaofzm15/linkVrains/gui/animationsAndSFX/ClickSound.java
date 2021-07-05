130b00aa8c939e8ff5f73828e08e3f24f1365d0c
joaofzm <joao.fzm@hotmail.com>
2021-07-05 17:02:33 -0300

    Update ClickSound.java

--- src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/ClickSound.java
+++ src/br/com/joaofzm15/linkVrains/gui/animationsAndSFX/ClickSound.java
@@ -3,7 +3,7 @@ package br.com.joaofzm15.linkVrains.gui.animationsAndSFX;
 public class ClickSound implements Runnable {
 
 	private SoundEffectConverter soundEffectConverter = new SoundEffectConverter();
-	String soundPath = "resources/sound effects/clickSound.wav";
+	String soundPath = "/sound effects/clickSound.wav";
 
 
 	@Override
